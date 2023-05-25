d98b436b7145847f89644f29d812d9af0d01b3ef
Igor Brandão <111445872+Punkrig@users.noreply.github.com>
2023-05-25 17:37:40 -0300

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from  'react-router-dom';
 import { Typography,Card, CardContent,CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
-import { demoThumbnailUrl,demoVideoUrl, demoVideoTitle, demoChannelUrl,demoChannelTitle } from '../utils/Contants';
+import { demoVideoUrl, demoVideoTitle, demoChannelUrl,demoChannelTitle } from '../utils/Contants';
 function VideoCard({video:{id:{videoId},snippet}}) {
     console.log(videoId, snippet)
   return (
